7be9520ce8eae181d54519c723b286854007288d
Minepas87 <macchiap03@gmail.com>
2022-12-06 21:06:13 +0100

    Fixed permissions and conditions in init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # check HDMI connection
-rm -f /home/pi/display_loader/name_of_hdmi.txt
-tvservice -n 2 > /home/pi/display_loader/name_of_hdmi.txt
+sudo rm -f /home/pi/display_loader/name_of_hdmi.txt
+sudo tvservice -v 2 -n > /home/pi/display_loader/name_of_hdmi.txt
 HDMI_NAME=`cat /home/pi/display_loader/name_of_hdmi.txt`
-if [ "$HDMI_NAME" == "[E] No device present" ]; then
+if [ "$HDMI_NAME" == "" ]; then
   # HDMI !connected
   LCD_ON=`cat /boot/config.txt | grep "ignore_lcd=1"`
   if [ "$LCD_ON" == "ignore_lcd=1" ]; then
     # reboot because LCD needs a specific configuration
     echo "reboot with LCD"
-    sudo cp -rf /home/pi/display_loader/LCD/config.txt /boot/config.txt
+    sudo cp -f /home/pi/display_loader/LCD/config.txt /boot/config.txt
     sudo reboot -n
   fi
 else
@@ -18,7 +18,7 @@ else
   if [ "$HDMI_ON" == "ignore_lcd=0" ]; then
     # rebbot beacuse HDMI needs a specific configuration
     echo "reboot with HDMI"
-    sudo cp -rf /home/pi/display_loader/HDMI/config.txt /boot/config.txt
+    sudo cp -f /home/pi/display_loader/HDMI/config.txt /boot/config.txt
     sudo reboot -n
   fi
 fi
